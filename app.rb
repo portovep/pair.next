9744e536f71c6ae65132cde58d0986549a0ae547
Martino Visintin <mvisinti@thoughtworks.com>
2014-03-04 18:14:33 +0000

    add the okta login skeleton

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
-require "sinatra"
+require 'bundler/setup'
+Bundler.require(:default)
+
+require 'openssl'
+require 'base64'
+
+require_relative './auth.rb'
+# ActiveRecord::Base.logger = Logger.new(STDERR)
+# ActiveRecord::Base.establish_connection(:adapter  => 'sqlite3', :database => 'db.sqlite')
+
+# require "sinatra"
 
 set :app_file, __FILE__
+
+before do
+  protected! unless request.path_info.include? '/auth'
+end
 
 get '/hi' do
   erb :index
