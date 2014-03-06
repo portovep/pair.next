3a501492b8065a221c27f444ed9eb91b31fe89b1
Lukas Ingelheim <lukas.ingelheim@gmail.com>
2014-03-06 14:38:11 +0100

    Lukas, Florian: getOldPairs now working with more than just one pairing

--- spec/team_unit_spec.rb
+++ spec/team_unit_spec.rb
@@ -16,11 +16,18 @@ describe 'Team class' do
       	team_membership = TeamMember.create(user: new_member, team: @team)
       end
 
-      TestUtilityMethods.create_pair("Lukas", "Florian")
-      TestUtilityMethods.create_pair("Pablo", "Martino")
+      start_time = Time.now
+      end_time = start_time + (10*60)
+      TestUtilityMethods.create_pair("Pablo", "Florian", start_time, end_time)
+      TestUtilityMethods.create_pair("Lukas", "Martino", start_time, end_time)
+
+      TestUtilityMethods.create_pair("Lukas", "Florian", end_time)
+      TestUtilityMethods.create_pair("Pablo", "Martino", end_time)
+
+
     end
 
-	it 'should get all old pairs' do
+	it 'should get the most current old pairs' do
 		old_pairs = @team.get_old_pairs
 		old_pairs.should be == [[User.find_by_username("Lukas"), User.find_by_username("Florian")],
 								[User.find_by_username("Pablo"), User.find_by_username("Martino")]]
--- spec/test_utility_methods.rb
+++ spec/test_utility_methods.rb
@@ -2,8 +2,8 @@ class TestUtilityMethods
 	def initialize()
 	end
 
-	def self.create_pair(name_one, name_two)
-	  pair = PairingSession.create()
+	def self.create_pair(name_one, name_two, start_time = nil, end_time = nil)
+	  pair = PairingSession.create(start_time: start_time, end_time: end_time)
       PairingMembership.create(user: User.find_by_username(name_one), pairing_session: pair)
       PairingMembership.create(user: User.find_by_username(name_two), pairing_session: pair)
 	end
