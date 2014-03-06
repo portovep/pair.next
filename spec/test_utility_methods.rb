6cbeeebbeee6d6c1ca16516e35befab6e6f3f0f3
Martino Visintin <mvisinti@thoughtworks.com>
2014-03-06 17:19:56 +0000

    refactor and add activerecord associations

--- spec/test_utility_methods.rb
+++ spec/test_utility_methods.rb
@@ -3,8 +3,10 @@ class TestUtilityMethods
 	end
 
 	def self.create_pair(name_one, name_two, start_time = nil, end_time = nil)
-	  pair = PairingSession.create(start_time: start_time, end_time: end_time)
-      PairingMembership.create(user: User.find_by_username(name_one), pairing_session: pair)
-      PairingMembership.create(user: User.find_by_username(name_two), pairing_session: pair)
+	  pairing_session = PairingSession.create(start_time: start_time, end_time: end_time)
+	  pairing_session.users << User.find_by_username(name_one)
+	  pairing_session.users << User.find_by_username(name_two)
+	  pairing_session.save
 	end
+
 end
