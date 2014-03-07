e22a521f01a308881fa6643df67ceda0fde95f8b
Florian Sellmayr <fsellmayr@thoughtworks.com>
2014-03-07 14:54:18 +0100

    prepared utility-methods to find next pairs

--- models/team.rb
+++ models/team.rb
@@ -2,7 +2,7 @@ class Team < ActiveRecord::Base
   has_many :team_members
   has_many :users, through: :team_members
   validates :name, presence: true, uniqueness: true
-
+  # TODO: not all those methods actually belong in team
   def get_old_pairs
     team_members = self.team_members
     pairs = {}
@@ -27,6 +27,18 @@ class Team < ActiveRecord::Base
     pairs.values
   end
 
+  def all_possible_pairings 
+    self.team_members.combination(2).to_a.map do |members|
+      members.map do |member|
+        member.user
+      end
+    end
+  end
+
+  def number_of_pairings_between(user1,user2)
+    pairing_memberships_with_user2 = user1.pairing_memberships.select { |membership| membership.pairing_session.users.include? user2}
+    pairing_memberships_with_user2.count
+  end 
 
   def shuffle_pairs
     team_members = self.team_members
