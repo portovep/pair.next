d3702579753c31d3bc1c54b95efe2a89446eddc9
portovep <portovep@tcd.ie>
2014-03-07 11:24:37 +0000

    Add user profile images by integrating gravatar

--- models/user.rb
+++ models/user.rb
@@ -1,7 +1,16 @@
+require 'digest/md5'
+
 class User < ActiveRecord::Base
   has_many :team_members
   has_many :teams, through: :team_members
 
   has_many :pairing_memberships
   has_many :pairing_sessions, through: :pairing_memberships
+
+  def image_url   
+    email_address = self.username.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    "http://www.gravatar.com/avatar/#{hash}"
+  end
+
 end
---
+++ spec/user_unit_spec.rb
@@ -0,0 +1,10 @@
+require_relative './test_helper.rb'
+
+describe 'User' do
+  
+  it 'should produce the right image link' do
+    user = User.new(username: 'vise890@gmail.com')
+    expect(user.image_url).to eq("http://www.gravatar.com/avatar/8d6f61601881ec2f053899a7732c59ba")
+  end
+
+end
