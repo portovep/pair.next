7fc6c295d9ed434b526e6c9cfc435a6ddb40f686
Martino Visintin <mvisinti@thoughtworks.com>
2014-03-10 09:41:25 +0000

    remove unneeded models

--- models/pairing_membership.rb
+++
@@ -1,4 +0,0 @@
-class PairingMembership < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :pairing_session
-end
--- models/pairing_session.rb
+++
@@ -1,4 +0,0 @@
-class PairingSession < ActiveRecord::Base
-  has_many :pairing_memberships
-  has_many :users, through: :pairing_memberships
-end
