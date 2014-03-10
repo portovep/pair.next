6c506d27548ddf4b9329333e91dc7266fa6775e9
Martino Visintin <mvisinti@thoughtworks.com>
2014-03-10 17:11:33 +0000

    refactor and tidy up

--- models/team.rb
+++ models/team.rb
@@ -6,16 +6,16 @@ class Team < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: true
 
-  def pair_up(pair)
-    self.pairing_sessions << PairingSession.create(users: pair)
-  end
-
   def possible_pairs
     users.combination(2).to_a.map(&:sort)
   end
 
-  # Return the number of times users have paired together
-  def pairing_frequency_of(users)
+  def pair_up(pair)
+    self.pairing_sessions << PairingSession.create(users: pair)
+  end
+
+  # Return the number of times pair has paired together in the current team
+  def pairing_frequency_of(pair)
     user_ids = users.map(&:id)
     where_query = user_ids.map{ |id| "#{id} = ANY(user_ids)"}.join(" AND ")
     self.pairing_sessions.where(where_query).count
@@ -37,10 +37,12 @@ class Team < ActiveRecord::Base
     # need to be careful with this; see
     # https://stackoverflow.com/questions/2698460/strange-ruby-behavior-when-using-hash-new
     pairing_frequency_table = Hash.new { [] }
+
     possible_pairs.each do |pair|
       frequency = pairing_frequency_of(pair)
       pairing_frequency_table[frequency] += [pair] # see above ^^
     end
+
     return pairing_frequency_table
   end
 
@@ -48,23 +50,26 @@ class Team < ActiveRecord::Base
     @next_pairs = []
 
     frequencies_table = pairing_frequency_table() # cache this and remove chosen/invalid pairs as we go
-    frequencies = frequencies_table.keys.sort # need this to access the hash in order
+    frequencies = frequencies_table.keys.sort # need this to access the hash in asc order
 
     frequencies.each do |frequency|
 
-      while next_pair = frequencies_table[frequency].shuffle!.pop # remove pairs as we visit them
-        if valid_pair?(next_pair) # need to check
+      loop do # we want to consider all the pairs at this frequency
+        next_pair = frequencies_table[frequency].shuffle!.pop # remove pairs form the table as we visit it
+        break if next_pair.nil? # if the pair is nil, we have finished the pairs at this frequency, move on
+        if valid_pair?(next_pair)
           @next_pairs << next_pair
         end
       end
 
     end
 
-    return @next_pairs.take(needed_pairs)
+    return @next_pairs.take(needed_pairs) # only take number of pairs we need
   end
 
   private
 
+  # ensures that no member of pair has already been assigned a pair in @next_pairs
   def valid_pair?(pair)
     chosen_users = @next_pairs.flatten
     !chosen_users.include?(pair.first) && !chosen_users.include?(pair.second)
@@ -73,4 +78,5 @@ class Team < ActiveRecord::Base
   def needed_pairs
     self.users.count / 2
   end
+
 end
