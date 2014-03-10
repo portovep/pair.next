7875b123a28eda4c94a6a612b5abfd5b1ea8e422
Martino Visintin <mvisinti@thoughtworks.com>
2014-03-10 13:22:30 +0000

    improve doc in user unit test

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -2,9 +2,11 @@ require_relative '../test_helper.rb'
 
 describe 'User' do
 
-  it 'should produce the right image link' do
-    user = User.new(username: 'vise890@gmail.com')
-    expect(user.image_url).to eq("http://www.gravatar.com/avatar/8d6f61601881ec2f053899a7732c59ba")
+  describe '#image_url' do
+    it 'should produce the right image link' do
+      user = User.new(username: 'vise890@gmail.com')
+      expect(user.image_url).to eq("http://www.gravatar.com/avatar/8d6f61601881ec2f053899a7732c59ba")
+    end
   end
 
 end
