52d9a3339810a68071fdd46b035cbc0067cee323
Martino Visintin <mvisinti@thoughtworks.com>
2014-03-10 13:47:40 +0000

    add preliminary frequency table

--- models/team.rb
+++ models/team.rb
@@ -6,4 +6,30 @@ class Team < ActiveRecord::Base
   def possible_pairs
     users.combination(2).to_a
   end
+
+  # returns a hash with the frequencies with which a pair
+  # has paired as keys and the pairs as values
+  #
+  # this is done for all the possible_pairs
+  #
+  # E.g.,
+  # { 0: [[florian pablo] [pablo tom]]
+  #   1: [[martino tom]]                    # [martino tom] have paired once
+  #   4: [[martino pablo] [florian lukas]]  # [martino pablo] and [florian lukas] have paired 4 times
+  #   .... and so on
+  # }
+  #
+  def pairing_frequencies
+    # this is somewhat expensive, so don't run it unnecessarily
+    return @pairing_frequencies if @pairing_frequencies
+
+    @pairing_frequencies = Hash.new([])
+    possible_pairs.each do |pair|
+      frequency_of_pair = PairingSession.frequency_of(pair)
+      @pairing_frequencies[frequency_of_pair] << pair
+    end
+
+    return @pairing_frequencies
+  end
+
 end
--- spec/unit/team_spec.rb
+++ spec/unit/team_spec.rb
@@ -2,18 +2,57 @@ require_relative '../test_helper.rb'
 
 describe 'Team' do
 
+  before(:each) do
+    @team = Team.create(name: 'uk-heroes') # sorry Germans, i needed an even number of people...
+
+  end
   describe '#possible_pairs' do
 
     it 'should give a list of all the possible pair combinations' do
-      team = Team.create(name: 'uk-heroes') # sorry Germans, i needed an even number of people...
       team_members = %w[Tom Martino Pablo John]
       team_members.each do |member_name|
-        team.users << User.create(username: member_name)
+        @team.users << User.create(username: member_name)
       end
 
       # FIXME: tests implementation?
-      expect(team.possible_pairs).to eq(team.users.combination(2).to_a)
+      expect(@team.possible_pairs).to eq(@team.users.combination(2).to_a)
     end
 
   end
+
+  describe '#pairing_frequencies' do
+    it 'should return a hash with the frequency at which each pair has occurred' do
+
+      florian = User.create(username: "Florian")
+      lukas = User.create(username: "lukas")
+      martino = User.create(username: "martino")
+      pablo = User.create(username: "pablo")
+      tom = User.create(username: "tom")
+
+      @team.users << florian
+      @team.users << lukas
+      @team.users << martino
+      @team.users << pablo
+      @team.users << tom
+
+      1.times do
+        PairingSession.create_with_users(users: [martino, tom])
+      end
+      4.times do
+        PairingSession.create_with_users(users: [martino, pablo])
+      end
+      4.times do
+        PairingSession.create_with_users(users: [florian, lukas])
+      end
+
+      pairing_frequencies = @team.pairing_frequencies
+
+      expect(pairing_frequencies[0]).to include([lukas, martino])
+      expect(pairing_frequencies[1]).to include([martino, tom])
+      expect(pairing_frequencies[4]).to include([martino, pablo])
+      expect(pairing_frequencies[4]).to include([florian, lukas])
+
+    end
+  end
+
 end
