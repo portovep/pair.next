7fef88e38d9ee9d916fdf8ebd3166e9aba2e80a1
Florian Sellmayr <fsellmayr@thoughtworks.com>
2014-03-11 17:55:35 +0100

    refactoring: removed duplication from user_unit_spec

--- spec/user_unit_spec.rb
+++ spec/user_unit_spec.rb
@@ -7,23 +7,24 @@ describe 'User' do
     expect(user.image_url).to eq("http://www.gravatar.com/avatar/8d6f61601881ec2f053899a7732c59ba")
   end
 
-  it 'should count the number of pairings with another user if pairings exist' do 
-    lukas = User.create(username: "Lukas")
-    florian = User.create(username: "Florian")
+  describe 'count pairings' do 
+    before (:each) do 
+      @lukas = User.create(username: "Lukas")
+      @florian = User.create(username: "Florian")
+      @pablo = User.create(username: "Pablo")
 
-    PairingSession.create(users: [lukas,florian])
+      PairingSession.create(users: [@lukas,@florian])
+    end
 
-    lukas.count_pairings_with(florian).should be == 1 
-  end
+    it 'should count the number of pairings with another user if pairings exist' do 
+      @lukas.count_pairings_with(@florian).should be == 1 
+      @florian.count_pairings_with(@lukas).should be == 1 
+    end
 
-  it 'should count the number of pairings with another user they didnt pair before' do 
-    lukas = User.create(username: "Lukas")
-    florian = User.create(username: "Florian")
-    pablo = User.create(username: "Pablo")
-
-    PairingSession.create(users: [lukas,florian])
-
-    lukas.count_pairings_with(pablo).should be == 0 
+    it 'should count the number of pairings with another user they didnt pair before' do 
+      @lukas.count_pairings_with(@pablo).should be == 0 
+      @pablo.count_pairings_with(@lukas).should be == 0
+    end
   end
 
 end
