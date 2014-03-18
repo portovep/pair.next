b58b6494afe65d5e84df4920548556936c94750c
Florian Sellmayr <fsellmayr@thoughtworks.com>
2014-03-18 18:06:29 +0100

    added simple vagrant/puppet config (everything still a bit broken)

---
+++ puppet/manifests/default.pp
@@ -0,0 +1,107 @@
+$ar_databases = ['pair_next']
+$as_vagrant   = 'sudo -u vagrant -H bash -l -c'
+$home         = '/home/vagrant'
+
+Exec {
+  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin']
+}
+
+# --- Preinstall Stage ---------------------------------------------------------
+
+stage { 'preinstall':
+  before => Stage['main']
+}
+
+class apt_get_update {
+  exec { 'apt-get -y update':
+    unless => "test -e ${home}/.rvm"
+  }
+}
+class { 'apt_get_update':
+  stage => preinstall
+}
+
+
+
+# --- PostgreSQL ---------------------------------------------------------------
+
+class install_postgres {
+  class { 'postgresql': }
+
+  class { 'postgresql::server': }
+
+  pg_database { $ar_databases:
+    ensure   => present,
+    encoding => 'UTF8',
+    require  => Class['postgresql::server']
+  }
+
+  pg_user { 'postgres':
+    ensure  => present,
+    password => 'postgres',
+    superuser => true,
+    require => Class['postgresql::server']
+  }
+
+  pg_user { 'pairnext':
+    ensure    => present,
+    password => 'pairnext',
+    superuser => true,
+    require   => Class['postgresql::server']
+  }
+
+  package { 'libpq-dev':
+    ensure => installed
+  }
+
+  package { 'postgresql-contrib':
+    ensure  => installed,
+    require => Class['postgresql::server'],
+  }
+}
+class { 'install_postgres': }
+
+
+# --- Packages -----------------------------------------------------------------
+
+package { 'curl':
+  ensure => installed
+}
+
+package { 'build-essential':
+  ensure => installed
+}
+
+package { 'git-core':
+  ensure => installed
+}
+
+# Nokogiri dependencies.
+package { ['libxml2', 'libxml2-dev', 'libxslt1-dev']:
+  ensure => installed
+}
+
+
+# --- Ruby ---------------------------------------------------------------------
+
+exec { 'install_rvm':
+  command => "${as_vagrant} 'curl -L https://get.rvm.io | bash -s stable'",
+  creates => "${home}/.rvm/bin/rvm",
+  require => Package['curl']
+}
+
+exec { 'install_ruby':
+  # We run the rvm executable directly because the shell function assumes an
+  # interactive environment, in particular to display messages or ask questions.
+  # The rvm executable is more suitable for automated installs.
+  #
+  # use a ruby patch level known to have a binary
+  command => "${as_vagrant} '${home}/.rvm/bin/rvm install ruby-2.0.0-p353 --binary --autolibs=enabled && rvm alias create default 2.0'",
+  creates => "${home}/.rvm/bin/ruby",
+  require => Exec['install_rvm']
+}
+
+exec { "${as_vagrant} 'gem install bundler --no-rdoc --no-ri'":
+  creates => "${home}/.rvm/bin/bundle",
+  require => Exec['install_ruby']
+}
