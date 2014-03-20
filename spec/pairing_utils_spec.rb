e76267de447ad991e1ad10df7eef68a7e2184a32
Florian Sellmayr <fsellmayr@thoughtworks.com>
2014-03-20 11:55:36 +0100

    seperated logic for valid/invalid pairing sessions from db access

--- spec/pairing_utils_spec.rb
+++ spec/pairing_utils_spec.rb
@@ -1,6 +1,6 @@
 require_relative './test_helper.rb'
 
-describe 'Team class' do
+describe 'PairingUtils' do
   before(:each) do
     @florian = { username: "florian"}
     @martino = { username: "martino"}
@@ -14,6 +14,17 @@ describe 'Team class' do
     PairingUtils.all_possible_pairs([@florian,@tom]).should match_array [[@florian,@tom]]
     PairingUtils.all_possible_pairs([@florian,@lukas,@pablo,@martino]).should match_array [
       [@florian, @lukas],[@lukas,@martino],[@lukas,@pablo],[@florian,@martino], [@florian,@pablo], [@pablo,@martino]]
+  end
 
+  it 'should detect an invalid pairing session if a user appears more than once' do
+    PairingUtils.is_valid_pairing_session([[@florian,@tom],[@florian,@martino]],[@florian,@tom,@martino]).should be == false
+  end
+
+  it 'should detect a valid pairing session if no user appears more than once' do 
+    PairingUtils.is_valid_pairing_session([[@florian,@tom],[@pablo,@martino]],[@florian,@tom,@martino,@pablo]).should be == true
+  end
+
+  it 'should detect if a teammember is not part of the pairing session' do 
+    PairingUtils.is_valid_pairing_session([[@florian,@tom]],[@florian,@tom,@martino,@pablo]).should be == false
   end
 end
