7f8e0fffc8d3f6486cba1f2ab6f38cdf6789e5f1
Martino Visintin <mvisinti@thoughtworks.com>
2014-03-21 16:36:54 +0000

    move unit tests

--- spec/unit/team_unit_spec.rb
+++ spec/unit/team_unit_spec.rb
@@ -1,18 +1,18 @@
-require_relative './test_helper.rb'
+require_relative '../test_helper.rb'
 
-describe 'Team class' do
+describe Team do
 
 
 	before(:each) do
     @team = Team.create(name: 'team_test')
-    
+
     @new_teammembers = ["Lukas", "Florian", "Pablo", "Martino"]
 
     @new_teammembers.each do |member|
     	new_member = User.create(username: member)
     	team_membership = TeamMember.create(user: new_member, team: @team)
     end
-    
+
     @lukas = User.find_by_username("Lukas")
     @florian = User.find_by_username("Florian")
     @pablo = User.find_by_username("Pablo")
@@ -38,12 +38,12 @@ describe 'Team class' do
     old_pairs.should be == []
   end
 
-  it 'should shuffle pairs when no previous pairings exist' do 
+  it 'should shuffle pairs when no previous pairings exist' do
     new_pairs = @team.shuffle_pairs
     new_pairs.size.should be == 2
     new_pairs[0].size.should be == 2
     new_pairs[1].size.should be == 2
-  end 
+  end
 
   it 'should generate only possible pairing solution left' do
     start_time = Time.now
@@ -59,11 +59,11 @@ describe 'Team class' do
                             [User.find_by_username("Florian"), User.find_by_username("Martino")]]
   end
 
-  it 'should generate a valid pairing solution when more than one option exists' do 
+  it 'should generate a valid pairing solution when more than one option exists' do
     TestUtilityMethods.create_pair("Lukas", "Florian")
     TestUtilityMethods.create_pair("Pablo", "Martino")
 
-    # valid pairing check: 
+    # valid pairing check:
     new_pairs = @team.shuffle_pairs
 
     new_pairs.count.should be == 2
@@ -75,10 +75,10 @@ describe 'Team class' do
     # todo: better checks?
   end
 
-  it 'should generate a hash of pairings grouped by start date as pairing history' do 
+  it 'should generate a hash of pairings grouped by start date as pairing history' do
     today = Time.current.utc.change(usec:0)
     yesterday = today - (24*60*60)
-    
+
     TestUtilityMethods.create_pair("Pablo", "Florian", yesterday, today)
     TestUtilityMethods.create_pair("Lukas", "Martino", yesterday, today)
 
@@ -93,21 +93,21 @@ describe 'Team class' do
     @team.pairing_history.should be == expected
   end
 
-  it 'should generate pairing statistics' do 
+  it 'should generate pairing statistics' do
     today = Time.now
     yesterday = today - (24*60*60)
     dayBefore = yesterday - (24*60*60)
     PairingSession.create(users: [@martino,@pablo], start_time: dayBefore, end_time: yesterday)
     PairingSession.create(users: [@lukas,@florian], start_time: dayBefore, end_time: yesterday)
 
-    PairingSession.create(users: [@martino,@pablo], start_time: yesterday, end_time: today) 
+    PairingSession.create(users: [@martino,@pablo], start_time: yesterday, end_time: today)
 
     @team.pairing_statistics.should be == {
       [@pablo,@martino] => 2,
       [@lukas,@florian] => 1,
-      [@lukas,@martino] => 0, 
-      [@lukas,@pablo] => 0, 
-      [@florian,@martino] => 0, 
+      [@lukas,@martino] => 0,
+      [@lukas,@pablo] => 0,
+      [@florian,@martino] => 0,
       [@florian,@pablo] => 0
     }
   end
