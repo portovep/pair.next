9a140948a6cea74a9115851499ece9fff808b94d
heyestom <heyestom@gmail.com>
2014-03-25 17:11:17 +0000

    new teams can no longer have current pairs

--- models/pairings.rb
+++ models/pairings.rb
@@ -3,7 +3,7 @@ class Pairing < ActiveRecord::Base
   has_many :users, through: :pairing_memberships
   belongs_to :team
   def self.find_current_by_team(team)
-    current_pairing_memberships = team.team_members.map { |member|   PairingMembership.find_current_by_user(member.user) }.select { |membership| membership != nil }
+    current_pairing_memberships = team.team_members.map { |member|   PairingMembership.find_current_by_user(member.user) }.select { |membership| membership != nil and membership.pairing.team_id == team.id}
     current_pairing_memberships.group_by { |membership| membership.pairing }.keys
   end
 end
