4c1196e52182d8fa528b5830a40d29ce0d923684
heyestom <heyestom@gmail.com>
2014-03-25 17:56:44 +0000

    fixed bug where team would never have current pairs :3

--- models/pairing_membership.rb
+++ models/pairing_membership.rb
@@ -2,12 +2,18 @@ class PairingMembership < ActiveRecord::Base
   belongs_to :user
   belongs_to :pairing
 
-  def self.find_by_team(team) 
+  def self.find_by_team(team)
     PairingMembership.find_by_sql ["select * from pairing_memberships where pairing_id in (select id from pairings where team_id = ?)",team.id]
   end
 
-  def self.find_current_by_user(user) 
+  def self.find_current_by_user(user)
     memberships = PairingMembership.where(user_id: user.id)
     memberships.find { |membership| membership.pairing.end_time == nil }
+  end
+
+
+  def self.find_current_by_user_and_team(user, team)
+    memberships = PairingMembership.where(user_id: user.id)
+    memberships.find { |membership| membership.pairing.team_id == team.id and membership.pairing.end_time == nil }
   end
 end
